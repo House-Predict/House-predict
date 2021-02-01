9feac43e4c475215eabb92b63cde379b1c593be3
prabhat-g97 <76996147+prabhat-g97@users.noreply.github.com>
2021-02-01 21:43:51 +0530

    Admin UI Fix

--- House-predict-dev/main/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Property,Feedback
-
-admin.site.register(Property)
-admin.site.register(Feedback)
--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Property,Feedback
+from django.contrib.auth.models import Group
 
-admin.site.register(Property)
-admin.site.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display=('name','date')
+    list_filter=('date',)
+    
+class PropertyAdmin(admin.ModelAdmin):
+    list_display=('id','user','location','price')
+
+admin.site.register(Property,PropertyAdmin)
+admin.site.register(Feedback,FeedbackAdmin)
+admin.site.unregister(Group)
+
+admin.site.site_header='House Predict Admin Dashboard'
